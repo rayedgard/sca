b7c05c9bbd9cab2f287a30d89e428881bc58e804
rayedgard <rayedgard@gmail.com>
2015-05-19 08:56:41 -0500

    agrega calse y form de manejo de fotograficas del trabajador

---
+++ sica/asistencia/foto.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace asistencia
+{
+    partial class foto
+    {
+        /// <summary> 
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar 
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbFoto = new System.Windows.Forms.PictureBox();
+            this.lbFecha = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbFoto
+            // 
+            this.pbFoto.Location = new System.Drawing.Point(6, 32);
+            this.pbFoto.Name = "pbFoto";
+            this.pbFoto.Size = new System.Drawing.Size(139, 126);
+            this.pbFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbFoto.TabIndex = 0;
+            this.pbFoto.TabStop = false;
+            // 
+            // lbFecha
+            // 
+            this.lbFecha.AutoSize = true;
+            this.lbFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFecha.ForeColor = System.Drawing.Color.Firebrick;
+            this.lbFecha.Location = new System.Drawing.Point(41, 11);
+            this.lbFecha.Name = "lbFecha";
+            this.lbFecha.Size = new System.Drawing.Size(80, 18);
+            this.lbFecha.TabIndex = 1;
+            this.lbFecha.Text = "22/08/2011";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.BackColor = System.Drawing.Color.White;
+            this.tbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNombre.ForeColor = System.Drawing.Color.Blue;
+            this.tbNombre.Location = new System.Drawing.Point(6, 164);
+            this.tbNombre.Multiline = true;
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.ReadOnly = true;
+            this.tbNombre.Size = new System.Drawing.Size(139, 58);
+            this.tbNombre.TabIndex = 2;
+            this.tbNombre.Text = "Nombre del muchcho el que vino";
+            this.tbNombre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightCyan;
+            this.groupBox1.Controls.Add(this.pbFoto);
+            this.groupBox1.Controls.Add(this.tbNombre);
+            this.groupBox1.Controls.Add(this.lbFecha);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(153, 230);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // foto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "foto";
+            this.Size = new System.Drawing.Size(153, 230);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbFoto;
+        private System.Windows.Forms.Label lbFecha;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ sica/asistencia/foto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace asistencia
+{
+    public partial class foto : UserControl
+    {
+        string aDNI = "";
+        public foto()
+        {
+            InitializeComponent();
+        }
+
+        public void AsignarFoto(string pDNI, string pNombres, string pFecha, Image pImagenFoto)
+        {
+            aDNI = pDNI;
+            tbNombre.Text = pNombres;
+            lbFecha.Text = pFecha;
+            pbFoto.Image = pImagenFoto;
+        }
+    }
+}
